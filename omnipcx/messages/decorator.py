abd3b41c5f565e5949ce9b91b43fa214ec96666d
Vlad Poenaru <vlad.wing@gmail.com>
2017-06-29 03:05:17 +0300

    removed unused stuff

--- omnipcx/messages/decorator.py
+++ omnipcx/messages/decorator.py
@@ -4,15 +4,7 @@ def MessageParameters(_type, size, _has_crc=True):
     def get_type(cls):
         return bytearray(_type, "ascii")
 
-    # def get_size(cls):
-    #     return size
-
-    # def has_crc(cls):
-    #     return _has_crc
-
     def _wrapper(cls):
         cls.get_type = classmethod(get_type)
-        # cls.has_crc = classmethod(has_crc)
-        # cls.get_size = classmethod(get_size)
         return cls
     return _wrapper
