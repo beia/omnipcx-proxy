9ab0cc0d198d48205b335d1e56f07c537551e9c0
Vlad Poenaru <vlad.wing@gmail.com>
2017-06-29 03:04:42 +0300

    track last message sent in proxy and close connections when silent for too long. Only change password in checkin message if password is empty

--- omnipcx/proxy.py
+++ omnipcx/proxy.py
@@ -1,8 +1,10 @@
 import threading
+import time
 from omnipcx.logging import Loggable
 from omnipcx.messages import MessageDetector
 from omnipcx.messages.protocol import SMDR, CheckInBase
 
+MAX_TIME = 60.0
 
 class Proxy(Loggable):
     def __init__(self, pbx, hotel, cdr, default_password):
@@ -15,12 +17,17 @@ class Proxy(Loggable):
         self.default_password = default_password
 
     def run(self):
+        time_last_recv =  {
+            "upstream": time.time(),
+            "downstream": time.time()
+        }
         upstream_g = self.upstream.messages()
         downstream_g = self.downstream.messages()
         while True:
             # Try to read from PBX
             u_msg = next(upstream_g)
             if u_msg:
+                time_last_recv["upstream"] = time.time()
                 self.logger.trace("Recv %s from pbx" % u_msg.serialize())
                 if isinstance(u_msg, SMDR):
                     self.cdr.send(u_msg.serialize_cdr())
@@ -30,17 +37,24 @@ class Proxy(Loggable):
                 if not d_msg:
                     self.logger.error("Timeout when waiting for message from Opera")
                     return
+                time_last_recv["downstream"] = time.time()
                 self.pbx.send(d_msg.serialize())
             # Try to read from Hotel
             d_msg = next(downstream_g)
             if d_msg:
+                time_last_recv["downstream"] = time.time()
                 self.logger.trace("Recv %s from hotel" % d_msg.serialize())
                 if isinstance(d_msg, CheckInBase):
-                    d_msg.password = self.default_password
+                    if d_msg.password == b"    ":
+                        d_msg.password = self.default_password
                 self.logger.trace("Send %s to pbx" % d_msg.serialize())
                 self.pbx.send(d_msg.serialize())
                 u_msg = next(upstream_g)
                 if not u_msg:
                     self.logger.error("Timeout when waiting for message from OLD/Hotel Driver")
                     return
+                time_last_recv["upstream"] = time.time()
                 self.hotel.send(u_msg.serialize())
+            if time.time() - max(time_last_recv.values()) > MAX_TIME:
+                self.logger.warn("The connections were innactive for too long. We are probably disconnected ...")
+                return
